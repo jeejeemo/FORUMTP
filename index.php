64597a89e4d158554d8b3726ffc4a6d07bc3343a
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-18 15:45:29 +0200

    commentaires dans index.php

--- index.php
+++ index.php
@@ -4,6 +4,17 @@ include_once("./model/fonctions.php");
 $page = getPage();
 
 
+/***
+ * Alfonso: Je vois que tu as eu des difficulté à appréhender la structure MVC.
+ * Il faut encore s'entrainer pour voir comment les fichiers intéragissent entre eux
+ * il est évident qu'il faut un peut d'entrainement pour absorber tout ça.
+ *
+ * Je pense que tu as compris le SQL mais il faut travailler le modèle MVC pour visualiser
+ * et ainsi être plus productif.
+ *
+ * Tu n'es pas le seul. Quelques personnes ont eu du mal avec ça. Il faut encore travailler!
+ */
+
 // if(!isset($_SESSION[id])){
 //     header('location:views/inscription.php');
 // }
